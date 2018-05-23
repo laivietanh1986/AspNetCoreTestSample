beee76fc021c65f7fcd0d71192271fc12e11c8c9
Thien Nguyen <nlqthien@gmail.com>
2018-05-23 16:59:16 +0700

    added product api tests

--- src/AspNetCoreSample/Data/Product.cs
+++ src/AspNetCoreSample/Data/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace AspNetCoreSample.Data
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public decimal Price { get; set; }
--- test/AspNetSample.E2ETests/ProductsApiTests.cs
+++ test/AspNetSample.E2ETests/ProductsApiTests.cs
@@ -1,11 +1,56 @@
-﻿using System;
+﻿using AspNetCoreSample;
+using AspNetCoreSample.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AspNetSample.E2ETests
 {
-    public class ProductsApiTests
+    public class ProductsApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        public ProductsApiTests(WebApplicationFactory<Program> factory)
+        {
+            Factory = factory;
+        }
+
+        public WebApplicationFactory<Program> Factory { get; }
+
+        [Fact]
+        public async Task CreateProductOk()
+        {
+            var client = Factory.CreateClient();
+            var product = new Product { Name = "Test product", Price = 10 };
+
+            var response = await client.PostAsJsonAsync("/api/products", product);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateProductReturnBadRequestWhenEmptyName()
+        {
+            var client = Factory.CreateClient();
+            var product = new Product { Name = "", Price = 10 };
+
+            var response = await client.PostAsJsonAsync("/api/products", product);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetProductsOk()
+        {
+            var client = Factory.CreateClient();
+
+            var response = await client.GetAsync("/api/products");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var products = await response.Content.ReadAsAsync<IList<Product>>();
+            Assert.NotEmpty(products);
+        }
     }
 }
