765c111860b114295242156fc6814c2c6313e1f3
Thien Nguyen <nlqthien@gmail.com>
2018-06-04 05:30:49 +0700

    add products pages

---
+++ src/AspNetCoreSample/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AspNetCoreSample.Data;
+
+namespace AspNetCoreSample.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AspNetCoreSample.Data.ApplicationDbContext _context;
+
+        public CreateModel(AspNetCoreSample.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ src/AspNetCoreSample/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreSample.Data;
+
+namespace AspNetCoreSample.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AspNetCoreSample.Data.ApplicationDbContext _context;
+
+        public IndexModel(AspNetCoreSample.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
